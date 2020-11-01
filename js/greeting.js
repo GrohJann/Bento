d20703cedf009270ba4e39bbb36c534b4aae6b7c
Jannis Jannis <jannismaxim@gmail.com>
2020-11-01 15:39:14 +0100

    greetings quick fix

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,6 @@
 // Get the hour
-var today = new Date();
-var Hr = today.getHours();
+var today2 = new Date();
+var Hr2 = today2.getHours();
 
 // Here you can change your name
 var name = 'Jannis';
@@ -14,12 +14,12 @@ var gree5 = 'Good evening ';
 var gree6 = 'Good evening ';
 
 // Define the hours of the greetings
-if (Hr >= 23 && Hr < 5) {
+if (Hr2 >= 23 && Hr2 < 5) {
     document.getElementById('greetings').innerText = gree1 + name;
-} else if (Hr >= 6 && Hr < 12) {
+} else if (Hr2 >= 6 && Hr2 < 12) {
     document.getElementById('greetings').innerText = gree2 + name;
-} else if (Hr >= 12 && Hr < 17) {
+} else if (Hr2 >= 12 && Hr2 < 17) {
     document.getElementById('greetings').innerText = gree3 + name;
-} else if (Hr >= 17 && Hr < 23) {
+} else if (Hr2 >= 17 && Hr2 < 23) {
     document.getElementById('greetings').innerText = gree4 + name;
 }
