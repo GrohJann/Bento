aebfdffe5f637c67acb0ac2c340dd6d2591402a6
Jannis Jannis <jannismaxim@gmail.com>
2020-11-01 01:40:35 +0100

    slight changes

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '94de48575de442c4d28149742e9aa0c9';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 49.006889;
+    let longitude = 8.403653;
 
     getWeather(latitude, longitude);
 }
@@ -51,7 +51,7 @@ function getWeather(latitude, longitude) {
 
 // Display Weather info
 function displayWeather() {
-    iconElement.innerHTML = `<img src="icons/OneDark/${weather.iconId}.png"/>`;
+    iconElement.innerHTML = `<img src="icons/White/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span class="darkfg">${tempUnit}</span>`;
     descElement.innerHTML = weather.description;
 }
